0f84b4822b5f6e2a50a76673ce667128717c4c8a
Abanoub Atef Ebeid Attia <abanoub.atef083@gmail.com>
2024-07-23 01:41:04 +0300

    do nothing

--- X-Vison/obj/Debug/Center_Maneger_Content.g.i.cs
+++ X-Vison/obj/Debug/Center_Maneger_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("img/password.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("img/cash.png")]
 
 
