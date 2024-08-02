a732ac0b1614785d7d1fc71b64e4db02c9266c75
Mostafa-Usama <mostafa.usama286@gmail.com>
2024-08-02 07:59:14 +0300

    wokred on adding order

--- X-Vison/View/Add_Order.xaml.cs
+++ X-Vison/View/Add_Order.xaml.cs
@@ -21,6 +21,7 @@ namespace Center_Maneger.View
     {
         int user_id;
         int chair_num;
+        Dictionary<int, int> basket = new Dictionary<int, int>();
 
         public Add_Order(int id, int chair)
         {
@@ -134,7 +135,110 @@ namespace Center_Maneger.View
 
         private void add_product(object sender, RoutedEventArgs e)
         {
-            
+            Button btn = sender as Button;
+            int productId = Convert.ToInt32(btn.Tag);
+            if (!basket.ContainsKey(productId))
+            {
+                basket.Add(productId, 1);
+                // لو button جديد
+                Tuple<string, int, double, double> products = databaseLoader.GetProductData(productId);
+                Button mainbtn = new Button();
+                //mainbtn.Click += add_product;
+                mainbtn.Margin = new Thickness(1);
+                mainbtn.BorderThickness = new Thickness(0);
+                mainbtn.Background = Brushes.LightGreen;
+              
+                //mainbtn.Tag = Convert.ToString(products[i].Item1);
+                Border border = new Border
+                {
+                    Height = 100,
+                    BorderBrush = Brushes.Black,
+                    Background = Brushes.LightGreen,
+                    BorderThickness = new Thickness(1),
+                    Margin = new Thickness(5),
+                    CornerRadius = new CornerRadius(5),
+                    HorizontalAlignment = HorizontalAlignment.Stretch,
+                    Name = "btn"+ productId.ToString(),
+                };
+                StackPanel stackPanel = new StackPanel();
+
+                TextBlock product_name = new TextBlock
+                {
+                    Text = products.Item1,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    FontWeight = FontWeights.Bold,
+                    FontSize = 18,
+                    Foreground = Brushes.Black,
+                };
+                TextBlock product_amount = new TextBlock
+                {
+                    Text = "الكمية: " + Convert.ToString(1),
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    FontWeight = FontWeights.Bold,
+                    FontSize = 18,
+                    Foreground = Brushes.Black,
+                    
+                };
+                TextBlock product_cost = new TextBlock
+                {
+                    Text = Convert.ToString(products.Item4) + " جنيه",
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    FontWeight = FontWeights.Bold,
+                    FontSize = 18,
+                    Foreground = Brushes.Black,
+                };
+                Button add = new Button
+                {
+                    Content = "+",
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    FontWeight = FontWeights.Bold,
+                   
+                    Foreground = Brushes.Black,
+                    Tag = productId.ToString(),
+                };
+                add.Click += add_product;
+                Image product_icon = new Image
+                {
+                    Width = border.Width * 0.3,
+                    Height = border.Height * 0.3,
+                    Source = new BitmapImage(new Uri("pack://application:,,,/img/kitchen icon2.png")),
+                };
+                stackPanel.Children.Add(product_icon);
+                stackPanel.Children.Add(product_name);
+                stackPanel.Children.Add(product_amount);
+                stackPanel.Children.Add(add);
+                stackPanel.Children.Add(product_cost);
+                mainbtn.Content = stackPanel;
+                border.Child = mainbtn;
+                bought_stack.Children.Add(border);
+            }
+            else
+            {
+                
+                basket[productId]++;
+                Border border = findChildBorder(productId);
+                Button clickedBtn = border.Child as Button;
+                StackPanel stack = clickedBtn.Content as StackPanel;
+                TextBlock amount = stack.Children[2] as TextBlock;
+                amount.Text = "الكمية: " + basket[productId].ToString();
+            }
+        }
+
+        private Border findChildBorder(int productId)
+        {
+             foreach (var child in bought_stack.Children)
+            {
+                Border x = child as Border;
+                if (x.Name == "btn"+productId.ToString())
+                {
+                    return x;
+                }
+            }
+            return null;
         }
 
         private void save_order(object sender, RoutedEventArgs e)
